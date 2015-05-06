b307f9eccdec3183b76cfdb104124aa2f7615d8e
fuyang <bishopmoveon@gmail.com>
2015-05-06 16:44:24 +0800

    add the model of school and Add a attribute to model lesson

--- Code/school/models.py
+++ Code/school/models.py
@@ -1,3 +1,18 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    """ The class 'School' indicate the school of sysu """
+    id = models.IntegerField(primary_key=True) # the id of school
+    chineseName = models.CharField(max_length=128)
+    englishName = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.chineseName
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'School'
+        cerbose_name = 'schools'
