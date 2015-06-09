b51d82fd3fac62f424c3f0888147b780251c5638
fuyang <bishopmoveon@gmail.com>
2015-06-09 23:57:59 +0800

    config views

--- Code/lesson/views.py
+++ Code/lesson/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Lesson
 
 # Create your views here.
+def lessons_list(request):
+    lessons = Lesson.objects.all()[:100]
+    return render(request, 'lesson/lessons_list.html', {'lessons': lessons})
+
+def lesson_detail(request, lesson_id):
+    try:
+        lesson = Lesson.objects.get(pk=lesson_id)
+    except Lesson.DoesNotExist:
+        raise Http404('Lesson does not exist...')
+    return render(request, 'lesson/lesson_detail.html', {'lesson': lesson})
