252bae8c3a2c6308f7f3796c913e2cae1e47533f
fuyang <bishopmoveon@gmail.com>
2015-06-09 23:57:36 +0800

    config URL

---
+++ Code/lesson/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+
+from lesson import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.lessons_list, name='lessons_list'),
+    url(r'^(?P<lesson_id>\d+)/$', views.lesson_detail, name='lesson_detail'),
+)
